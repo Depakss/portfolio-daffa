38ae111c4d5a6be0c2cfb5466bd870a09a4c4348
daffanf <daffanaufalf@gmail.com>
2023-08-26 11:03:13 +0700

    first commit
    yeah bois

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  darkMode: "media",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        merah: "#ff4b57",
+        hitam: "#23253a",
+        ijo: "#cbf380",
+        ungu: "#4831d4",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
